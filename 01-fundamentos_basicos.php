f1b039a362fdacc845fe305f0eca18df7ed445ba
Vinicius Rolim Barbosa <viiniirb@proton.me>
2024-09-18 01:26:37 -0400

    Add PHP code for basic fundamentals and variables

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Fundamentos basicos</title>
+    </head>
+    <body>
+        <?php // Tags de abertura e fechamento do PHP
+            echo "Meu nome é: <strong>Vinicius</strong><br>";
+            echo "Minha idade é: <strong>25</strong>";
+        ?>
+    </body>
+
+</html>
---
+++ 02-variaveis.php
@@ -0,0 +1,30 @@
+<?php
+    // Meus dados
+    $nome = "Vinicius";
+    $idade = 25;
+    $altura = 1.78;
+    $casado = false;
+
+    // Exibindo os dados
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura.<br>";
+
+    // Alterando os dados
+    $nome = "Vinicius Rolim";
+    $idade = 26;
+    $altura = 1.80;
+    $casado = true;
+
+    // Exibindo os dados
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura.<br>";
+
+    /* 
+        Regras para criação de variáveis:
+        - Não começar com números
+        - Não utilizar caracteres especiais
+        - Não utilizar acentos
+        - Não utilizar espaços
+        - Utilizar o padrão camelCase
+        - As variáveis são case-sensitive
+        - Não utilizar palavras reservadas
+    */
+?>
